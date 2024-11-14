51a690730eabb724db46db554f2192de1d476b3f
dhkim <cvasq2103@gmail.com>
2024-11-14 15:33:36 +0900

    커밋테스트

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/common/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'; // 리액트 부트스트랩 임포트
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header />
   );
 }
 
