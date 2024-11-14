18d50afb8de553d93f736a0963b9da8d4200f08f
dhkim <cvasq2103@gmail.com>
2024-11-14 17:24:56 +0900

    1. App.js 화면 출력 수정 2. TagComponents 임포트 경로 수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Header from './components/common/Header';
-import 'bootstrap/dist/css/bootstrap.min.css'; // 리액트 부트스트랩 임포트
+import "./App.css";
+import Layout from "./components/common/Layout"; // 레이아웃 컴포넌트 import
+import { Container } from 'react-bootstrap';
+import TwoGrids from './components/common/TwoGrids';
+import "bootstrap/dist/css/bootstrap.min.css"; // 리액트 부트스트랩 임포트
+import MainCards from './components/common/MainCards';
 
 function App() {
   return (
-    <Header />
+    <Layout>
+      <Container className="my-4">
+        <TwoGrids />
+      </Container>
+
+      <Container className="my-4">
+        <MainCards />
+      </Container>
+    </Layout>
   );
 }
 export default App;
