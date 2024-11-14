ea6a91e49a057be8aa5200cc4ced0ee68f85f884
dhkim <cvasq2103@gmail.com>
2024-11-14 14:20:00 +0900

    커밋 테스트

---
+++ my-app/src/components/common/Header.jsx
@@ -0,0 +1,15 @@
+// components/common/Header.jsx
+import React from 'react'
+
+const header = () => {
+    
+    function item() {
+        
+    }
+
+  return (
+    <div>header</div>
+  )
+}
+
+export default header;
