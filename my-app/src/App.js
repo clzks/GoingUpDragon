7fca97ef9e65a11ba70853919cf515f7195a4a2b
LSJEONG-1 <showlee00@gmail.com>
2024-11-15 09:59:40 +0900

    카드아이템, 카즈, 메인카즈 컴포넌트 컨벤션에 맞춰 수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,15 @@
-import "./App.css";
-import Layout from "./components/common/Layout"; // 레이아웃 컴포넌트 import
-import { Container } from 'react-bootstrap';
-import TwoGrids from './components/common/TwoGrids';
-import "bootstrap/dist/css/bootstrap.min.css"; // 리액트 부트스트랩 임포트
-import MainCards from './components/common/MainCards';
+// App.js
+// 외부 라이브러리
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap 스타일 추가
+import MainCards from "./components/common/MainCards"; // MainCards 컴포넌트 임포트
 
 function App() {
   return (
-    <Layout>
-      <Container className="my-4">
-        <TwoGrids />
-      </Container>
-
-      <Container className="my-4">
-        <MainCards />
-      </Container>
-    </Layout>
+    <div className="App">
+      <MainCards />
+    </div>
   );
 }
+
 export default App;
