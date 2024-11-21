65d4a4888f62de019dcffeaaaddf6b4256c3cceb
dhkim <cvasq2103@gmail.com>
2024-11-21 13:36:36 +0900

    1. 정렬 옵션인 SortOption.jsx 컴포넌트 components/common 에 추가

---
+++ my-app/src/components/common/SortOption.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const SortOption = () => {
+  return (
+    <ButtonGroup>
+      <Button>1</Button>
+      <Button>2</Button>
+
+      <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-nested-dropdown">
+        <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+        <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+      </DropdownButton>
+    </ButtonGroup>
+  );
+};
+export default SortOption;
