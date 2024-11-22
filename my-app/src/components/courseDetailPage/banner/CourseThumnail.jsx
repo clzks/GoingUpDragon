c4b474dfe4c0b93a1f17c40cf5af07ab4c67ad70
clzks <clzks@naver.com>
2024-11-22 17:25:39 +0900

    강의상세페이지 배너 작업중  - 강사명 , 태그 남음

--- my-app/src/components/courseDetailPage/banner/CourseThumnail.jsx
+++ my-app/src/components/courseDetailPage/banner/CourseThumnail.jsx
@@ -7,11 +7,11 @@ const CourseThumnail = () => {
 
 const StyledThumnail = styled.div`
   position: absolute; /* 부모를 기준으로 위치 지정 */
-  width: 640px;
-  height: 360px;
+  width: 368px;
+  height: 240px;
   background-color: white;
   right: 360px; /* 부모의 오른쪽 끝에서 10px 떨어짐 */
-  top: 200px; /* 부모의 위쪽에서 20px 떨어짐 */
+  top: 120px; /* 부모의 위쪽에서 20px 떨어짐 */
   border-radius: 10px;
 `;
 
--- my-app/src/components/courseDetailPage/banner/DetailCourseBanner.jsx
+++ my-app/src/components/courseDetailPage/banner/DetailCourseBanner.jsx
@@ -2,47 +2,111 @@ import React from "react";
 import styled from "styled-components";
 import CourseThumnail from "./CourseThumnail";
 import { Container } from "react-bootstrap";
+import RatingStar from "../../common/icons/RatingStar";
+import { FaUser } from "react-icons/fa";
 
 const DetailCourseBanner = ({ lectureData, enrollmentData }) => {
   const { mainCategory, secondaryCategory, title, keyPoint, tags } =
     lectureData;
   const { instructor, rating, reviews, students } = enrollmentData;
 
+  const reviewText = ` 수강평 ${reviews.toLocaleString()}개`;
+  const studentText = ` 수강생 ${students.toLocaleString()}명`; // 천 단위로 , 추가를 위해 toLocaleString 사용
+
   return (
-    <div>
-      <StyledBanner>
+    <StyledBannerBackground>
+      <StyledBannerContainer>
         <CourseThumnail></CourseThumnail>
-        <StyledCategoryText>
-          {mainCategory} / {secondaryCategory}
-        </StyledCategoryText>
-        <StyledBannerText>ㅇㅇ</StyledBannerText>
-        <StyledBannerText>ㅇㅇ</StyledBannerText>
-      </StyledBanner>
-    </div>
+        <StyledCategoryTextContainer>
+          <StyledCategoryText>{mainCategory}</StyledCategoryText>/
+          <StyledCategoryText>{secondaryCategory}</StyledCategoryText>
+        </StyledCategoryTextContainer>
+        <StyledBannerTitle>{title}</StyledBannerTitle>
+        <StyledBannerKeyPoint>{keyPoint}</StyledBannerKeyPoint>
+        <StyledEnrollmentContainer>
+          <RatingStar></RatingStar>
+          <StyledEnrollmentText>{rating}</StyledEnrollmentText>
+          <StyledReviewText>{reviewText}</StyledReviewText>
+          <StyledEnrollmentText>{studentText}</StyledEnrollmentText>
+        </StyledEnrollmentContainer>
+        <StyledFaUser></StyledFaUser>
+      </StyledBannerContainer>
+    </StyledBannerBackground>
   );
 };
 
-const StyledBanner = styled(Container)`
-  min-height: 900px;
+const StyledBannerBackground = styled.div`
+  background-color: black;
+  min-height: 500px;
+  height: auto;
   position: relative;
-  flex-direction: column;
   color: white;
 `;
 
-const StyledBannerText = styled.span`
-  justify-content: flex-start; /* 왼쪽 정렬 */
-  padding-left: 100px; /* 왼쪽 여백 */
-  margin-top: 20px;
+const StyledBannerContainer = styled(Container)`
+  flex-direction: column;
+  display: flex;
 `;
 
-const StyledCategoryText = styled(StyledBannerText)`
-  margin-top: 80px;
+const StyledCategoryTextContainer = styled.div`
+  margin-top: 60px;
+  display: flex;
+  padding-left: 45px; /* 왼쪽 여백 */
+`;
+
+const StyledCategoryText = styled.span`
   color: gray;
-  font-size: 30px;
+  font-size: 20px;
+  text-decoration: underline; /* 테스트 밑줄 */
+  text-underline-offset: 4px; /* 간격 설정 */
+  padding-right: 5px;
+  padding-left: 5px;
+`;
+
+const StyledBannerText = styled.span`
+  margin-left: 50px;
+  margin-top: 20px;
+  justify-content: flex-start; /* 왼쪽 정렬 */
 `;
 
 const StyledBannerTitle = styled(StyledBannerText)`
-  margin-top: 150px;
+  font-size: 40px;
+  width: 640px; /* 컨테이너 너비 */
+  overflow-wrap: break-word; /* 단어를 강제로 줄바꿈 */
+  word-break: keep-all; /* 띄어쓰기가 없는 단어는 한 줄에 유지 */
+`;
+
+const StyledBannerKeyPoint = styled(StyledBannerText)`
+  font-size: 20px;
+  width: 640px;
+  overflow-wrap: break-word; /* 단어를 강제로 줄바꿈 */
+  word-break: keep-all; /* 띄어쓰기가 없는 단어는 한 줄에 유지 */
+  color: gray;
+`;
+
+const StyledEnrollmentContainer = styled.div`
+  padding-left: 50px; /* 왼쪽 여백 */
+  display: flex;
+  margin-top: 20px;
+  align-items: center;
+`;
+
+const StyledEnrollmentText = styled.span`
+  font-size: 20px;
+  padding-left: 8px;
+`;
+
+const StyledReviewText = styled.button`
+  background: transparent; /* 배경 투명 */
+  border: none; /* 테두리 제거 */
+  padding-left: 10px;
+  text-decoration: underline; /* 테스트 밑줄 */
+  text-underline-offset: 3px; /* 간격 설정 */
+  color: white;
+`;
+
+const StyledFaUser = styled(FaUser)`
+  margin-left: 50px;
 `;
 
 export default DetailCourseBanner;
