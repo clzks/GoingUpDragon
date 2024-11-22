d6ef1a27c2ee6a24883eee31fc5884934dbf7652
dhkim <cvasq2103@gmail.com>
2024-11-22 14:36:19 +0900

    메인 가로선 세로선 수정하기위한 커밋

--- my-app/src/layouts/searchPage/SearchLayout.jsx
+++ my-app/src/layouts/searchPage/SearchLayout.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import SearchSortOption from '../../components/searchPage/SearchSortOption';
+// layouts/searchPage/SearchLayout.jsx
+import React from "react";
 
-const SearchLayout = () => { 
+// 외부 라이브러리
+import styled from "styled-components";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// 공통 컴포넌트
+import Layout from "../../components/common/layout/Layout";
+import SearchCategory from "../../components/searchPage/SearchCategory";
+
+// 검색 페이지 컴포넌트
+
+const SearchLayout = () => {
   return (
-    <SearchSortOption></SearchSortOption>
-  )
-}
+    <>
+      <Layout>
+        <StyledSection>
+          <Container>
+            <Row>
+              <Col xs={1}>
+                <SearchCategory></SearchCategory>
+              </Col>
+            </Row>
+          </Container>
+        </StyledSection>
+      </Layout>
+    </>
+  );
+};
 
 export default SearchLayout;
+
+const StyledSection = styled.section`
+  padding: 16px;
+`;
+
+// const StyledCol = styled(Col)`
+//   display: flex;
+//   align-items: center; /* 가운데 정렬 */
+// `;
