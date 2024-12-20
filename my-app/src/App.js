d93ba305eb78a08485b7eab82d1d2600a22d309b
donghwikim-create <cvasq2103@gmail.com>
2024-12-20 18:38:38 +0900

    App.js 페이지 이동 구현현

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,45 @@
 // App.js
+
+// 전역 스타일
 import "./App.css";
+
+// 외부 라이브러리리
 import "bootstrap/dist/css/bootstrap.min.css"; // 리액트 부트스트랩 임포트
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// GoingUpDragon\my-app\src
 import MainLayout from "./layouts/mainPage/MainLayout";
 import CourseDetailLayout from "./layouts/courseDetailPage/courseDetailLayout";
 import MyPageLayout from "./layouts/myPage/MyPageLayout";
 import SearchLayout from "./layouts/searchPage/SearchLayout";
 import PolicyLayout from "./layouts/policyPage/PolicyLayout";
-import LoginModal from "./components/common/utilities/LoginModal";
-import AnswerLayout from "./layouts/qnAPage/answer/AnswerLayout";
-import QnAMainLayout from "./layouts/qnAPage/main/QnAMainLayout";
 import SignupPage from "./layouts/signUpPage/SignUpLayout";
+import LoginModal from "./components/common/utilities/LoginModal";
 
 function App() {
-  return <MainLayout></MainLayout>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">메인 페이지</Link></li>
+            <li><Link to="/Search">검색 페이지</Link></li>
+            <li><Link to="/CourseDetail">강의 상세 페이지</Link></li>
+            <li><Link to="/MyPage">마이 페이지</Link></li>
+            <li><Link to="/Policy">약관 페이지</Link></li>
+            <li><Link to="/SignUp">회원가입 페이지</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<MainLayout />} />
+          <Route path="/Search" element={<SearchLayout />} />
+          <Route path="/CourseDetail" element={<CourseDetailLayout />} />
+          <Route path="/MyPage" element={<MyPageLayout />} />
+          <Route path="/Policy" element={<PolicyLayout />} />
+          <Route path="/SignUp" element={<SignupPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 export default App;
